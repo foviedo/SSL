12ca49bda69256927a8b16a2c9f6a37ea7ee7b87
foviedo <oviedoee@yahoo.com>
2019-04-08 19:23:36 -0300

    dsd

--- 00-CHelloWorld/src/hello.c
+++ 00-CHelloWorld/src/hello.c
@@ -1,14 +1,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-char direccion[] = "output.txt";
 
-//fp = fopen("output.txt","w");
 int main(void) {
-	FILE *fp = fopen(direccion,"w");
-	//puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	fprintf(stdout,"hola, mundo\n");
-	fprintf (fp,"hola, mundo\n" );
 
 	return EXIT_SUCCESS;
 }
